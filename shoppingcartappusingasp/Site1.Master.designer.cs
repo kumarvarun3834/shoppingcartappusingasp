007c19aadbd1db3cbd435abea94cce516612dac3
kumarvarun3834 <154826151+kumarvarun3834@users.noreply.github.com>
2025-12-18 17:42:37 +0530

    redirection bug fixed

--- shoppingcartappusingasp/Site1.Master.designer.cs
+++ shoppingcartappusingasp/Site1.Master.designer.cs
@@ -67,6 +67,24 @@ namespace shoppingcartappusingasp {
         protected global::System.Web.UI.WebControls.Panel GuestPanel;
         
         /// <summary>
+        /// A1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
+        
+        /// <summary>
+        /// A2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A2;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
